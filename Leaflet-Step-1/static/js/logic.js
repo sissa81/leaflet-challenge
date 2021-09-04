ee1e518f7b9ef8f3ea29d1986f7893bef0590357
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-04 14:39:27 -0700

    Base Grayscale

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,39 +1,31 @@
 // Create variable for url
 var earthquakedata = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
-// Define a map object.
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5,
-    layers: []
-  });
-
-// TESTING GRAYSCALE MAPS
-  var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+// Base layer map - grayscale found on github, https://github.com/leaflet-extras/leaflet-providers/issues/316, http://leaflet-extras.github.io/leaflet-providers/preview/#filter=CartoDB.Positron
+var street = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 });
 
-var grayscale = L.tileLayer.grayscale('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 14, minZoom: 2
-}).
+// Base layer variable in case I add more than just 'street'
+var baseMaps = {
+  Street: street,  
+};
+
+// Define a map object.
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [street]
+});
+
+// Pass our map layers into our layer control.
+// Add the layer control to the map.
+L.control.layers(baseMaps).addTo(myMap);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
 
 
-// TESTING LEAFLET TEMPLATE FOR GRAYSCALE, TILELAYER AND MARKERS
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
-    
-
-
-
-
-// Perform an API call to get the earthquake data. Call createMarkers when it completes.
-d3.json(earthquakedata).then(createMarkers);
+// // Perform an API call to get the earthquake data. Call createMarkers when it completes.
+// d3.json(earthquakedata).then(createMarkers);
